df103b692e3924cd25537e8ad1709f3b07169c91
Raheel <frommusafirshan@gmail.com>
2021-12-07 00:46:36 +0500

    Assets fix

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('assets/css/styles.css') }}" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
 </head>
